c7c77587a6b9ff67cb2d2a3baedc3273de1326ee
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-08-29 12:53:42 +0200

    card-shuffle

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,8 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
   pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   pickCard() {
     this.pickCardAnimation = true;
